6865e98eee0646386c688542e594ad9fa2c91ecf
wvinicius-eng <w.vinicius@escolar.ifrn.edu.br>
2026-08-28 14:01:54 -0300

    Operadores aritimeticos

--- scripts/appjs.js
+++ scripts/appjs.js
@@ -1,6 +1,7 @@
+//entrada e saida 
 let nasc = prompt("Digite seu ano de nascimento: ")
 
-nasc = parseInt(nasc);
+nasc = parseInt(nasc); //parseint converter string em inteiro
 
 let viva = confirm("se você esta viva> clique em ok.") //confirm tem um campo de texto pra vc confirmar
 
---
+++ scripts/appjs2.js
@@ -0,0 +1,10 @@
+let n1 = prompt("Digite um número:");
+n1 = Number(n1);
+let n2 = prompt("Digite o numero: ");
+n2 = Number(n2);
+alert(`${n1} + ${n2} = ${n1 + n2}`);
+alert(`${n1} - ${n2} = ${n1 - n2}`);
+alert(`${n1} / ${n2} = ${n1 / n2}`);
+alert(`${n1} % ${n2} = ${n1 % n2}`);
+alert(`${n1} * ${n2} = ${n1 * n2}`);
+alert(`${n1} ** ${n2} = ${n1 ** n2}`);
