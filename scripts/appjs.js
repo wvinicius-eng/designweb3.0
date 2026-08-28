2e64de645a7d35f6ca2e779bbebdca50644fc817
wvinicius-eng <w.vinicius@escolar.ifrn.edu.br>
2026-08-28 13:47:54 -0300

    exemplo in and out

---
+++ scripts/appjs.js
@@ -0,0 +1,11 @@
+let nasc = prompt("Digite seu ano de nascimento: ")
+
+nasc = parseInt(nasc);
+
+let viva = confirm("se você esta viva> clique em ok.") //confirm tem um campo de texto pra vc confirmar
+
+if(viva){
+    alert(`voce trem ${2026-nasc} anos.`)
+}else{
+    alert("voce morreu.")
+}
