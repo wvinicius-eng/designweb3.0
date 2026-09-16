cbfd8e420005713696e968508e3a7b6be8006f8b
wvinicius-eng <w.vinicius@escolar.ifrn.edu.br>
2026-09-16 17:54:58 -0300

    exemplo

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,3 +1,4 @@
+
 let i = 1;
 
 const imagem = document.getElementById("foto");
@@ -27,3 +28,49 @@ function imagemAnterior(e) {
 botaoProx.addEventListener("click", proximaImagem);
 botaoAnt.addEventListener("click", imagemAnterior);
 
+const cbbox = document.getElementById("combobox");
+
+function selecionar(e) {
+    e.preventDefault();
+
+    i = Number(cbbox.value);
+
+    imagem.setAttribute("src", `./pets/img/foto${i}.jpg`);
+}
+
+cbbox.addEventListener("change", selecionar);
+
+const opacidade = document.getElementById("opacidade");
+
+opacidade.addEventListener("input", function(e) {
+    e.preventDefault();
+
+    let valor = opacidade.value;
+
+    imagem.setAttribute("style", `opacity:${valor}`);
+});
+
+const li1 = document.getElementById("li1");
+const li2 = document.getElementById("li2");
+const li3 = document.getElementById("li3");
+
+li1.addEventListener("mouseover", function(e) {
+    e.preventDefault();
+
+    i = 1;
+    imagem.setAttribute("src", `./pets/img/foto${i}.jpg`);
+});
+
+li2.addEventListener("mouseover", function(e) {
+    e.preventDefault();
+
+    i = 2;
+    imagem.setAttribute("src", `./pets/img/foto${i}.jpg`);
+});
+
+li3.addEventListener("mouseover", function(e) {
+    e.preventDefault();
+
+    i = 3;
+    imagem.setAttribute("src", `./pets/img/foto${i}.jpg`);
+});
