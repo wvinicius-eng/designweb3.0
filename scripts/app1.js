4e33205a0fcca7a1a44a16f8439c0246b38274e2
wvinicius-eng <w.vinicius@escolar.ifrn.edu.br>
2026-09-18 14:24:57 -0300

    arrays

---
+++ scripts/app1.js
@@ -0,0 +1,9 @@
+const amigos = ["Pedro", "Ana", "Clara"];
+
+tam = amigos.length;
+
+console.log(`arranjo ${amigos}`);
+console.log(`tamanho ${tam}`);
+console.log(`Primeiro ${amigos[0]}`);
+console.log(`ultimo ${amigos[-1]}`)
+
